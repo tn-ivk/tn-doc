84b73f7e384f7cbf5645f0c6a6b8fdea4f4d237e
kondratyev.ie <work.ik@yandex.ru>
2023-09-04 08:41:16 +0300

    тест принтеров

--- TN_Doc/Controllers/PrintController.cs
+++ TN_Doc/Controllers/PrintController.cs
@@ -13,10 +13,7 @@ namespace TN_Doc.Controllers
 
         public List<string> GetListPrinters()
         {
-            //var installedPrinters = PrinterSettings.InstalledPrinters;
-
             List<string> printers = new List<string>();
-
             var process = new Process()
             {
                 StartInfo =
@@ -27,41 +24,24 @@ namespace TN_Doc.Controllers
                     WindowStyle = ProcessWindowStyle.Hidden,
                     Arguments = "-e",
                     FileName = "lpstat",
-                    CreateNoWindow = true
+                    CreateNoWindow = true,
+                    
                 }
             };
-            try
-            {
-                process.Start();
-                process.WaitForExit();
-
-                var resultOutput = process.StandardOutput.ReadToEnd();
-
-                foreach (var item in resultOutput.Split('\n'))
-                    if (!string.IsNullOrEmpty(item))
-                        printers.Add(item);
-
-                return printers;
-            }
-            catch (Exception e)
-            {
-                return new List<string>();
-            }
+            process.Start();
+            process.WaitForExit();
+            var resultOutput = process.StandardOutput.ReadToEnd();
+            foreach (var item in resultOutput.Split('\n'))
+                if (!string.IsNullOrEmpty(item))
+                    printers.Add(item);
+            
+            return printers;
         }
 
         public void PrintDoc(string printerName)
         {
-            //Process process = new Process();
-            //// Configure the process using the StartInfo properties.
-            //process.StartInfo.FileName = "lpr";
-            //process.StartInfo.Arguments = "-H localhost -P  RICOH_P_C200W_I -ol /opt/TN_Doc/wwwroot/PDF/PDF.pdf";
-            //process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            //process.Start();
-
             var printersName = GetListPrinters();
-
             Console.WriteLine($"-H localhost -P  {printerName} -ol {Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot", "PDF", $"PDF.pdf")}");
-
             if (printersName.Contains(printerName))
             {
                 var process = new Process()
@@ -78,10 +58,14 @@ namespace TN_Doc.Controllers
                 };
                 process.Start();
                 process.WaitForExit();
-
-                //_logger.LogInformation(process.StandardOutput.ReadToEnd());
-                // _logger.LogInformation(process.StandardError.ReadToEnd());
             }
+            
+            /// printer service
+            /// printerHelper________
+            /// |                    |
+            /// WindowsPrinterHelper |
+            ///                      LinuxPrinteHelper 
+            
         }
     }
 }
