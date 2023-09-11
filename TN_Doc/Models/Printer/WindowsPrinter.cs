171811b925da1f05c68296f64a2604dd56d7a91a
kondratyev.ie <work.ik@yandex.ru>
2023-09-11 17:21:00 +0300

    drop depedences

--- TN_Doc/Models/Printer/WindowsPrinter.cs
+++ TN_Doc/Models/Printer/WindowsPrinter.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing.Printing;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Process = PDFtoPrinter.Process;
 
 
 namespace TN_Doc.Models.Printer
