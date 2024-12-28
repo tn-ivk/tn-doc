d790ce615fa063fa4a8c452cd1752581cc49d996
DadonovAleksandr <a.snafu@ya.ru>
2024-12-28 14:57:34 +0300

    Рефакторинг HomeController

---
+++ TN_Doc/Models/Home/Data.cs
@@ -0,0 +1,12 @@
+﻿using TN.DocData;
+
+namespace TN_Doc.Models.Home
+{
+    public class Data
+    {
+        public int IdDevice { get; set; }
+        public IdDoc IdDoc { get; set; }
+        public string DTBegin { get; set; }
+        public string DTEnd { get; set; }
+    }
+}
---
+++ TN_Doc/Models/Home/ListItem.cs
@@ -0,0 +1,8 @@
+﻿namespace TN_Doc.Models.Home
+{
+    public class ListItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
