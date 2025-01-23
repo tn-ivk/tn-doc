b5dcf3480edeb46717d1191bae32c2dd4bd05baf
DadonovAleksandr <a.snafu@ya.ru>
2025-01-23 13:02:47 +0300

    перенес класс Method в Common.js

--- TN_Doc/wwwroot/js/Common.js
+++ TN_Doc/wwwroot/js/Common.js
@@ -1362,3 +1362,24 @@ function ClearDataElis() {
     $('#listPassports').empty();
     localStorage.removeItem('dataPassport');
 }
+
+class Metod
+{
+    Id;
+    Use;
+    IdParameter;
+    Name;
+    LimitValueActivate;
+    LimitValue;
+    LimitValueString;
+
+    constructor(pId, pUse, pIdParameter, pName, pLimitValueActivate, pLimitValue, pLimitValueString) {
+        this.Id = pId;
+        this.Use = pUse;
+        this.IdParameter = pIdParameter;
+        this.Name = pName;
+        this.LimitValueActivate = pLimitValueActivate;
+        this.LimitValue = pLimitValue;
+        this.LimitValueString = pLimitValueString;
+    }
+}
