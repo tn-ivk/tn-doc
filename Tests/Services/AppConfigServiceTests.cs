7cc41ea990d003a442faa418da9605259925a3e7
DadonovAleksandr <a.snafu@ya.ru>
2025-03-22 13:09:07 +0300

    Отладка тестов для AppConfigService

--- Tests/Services/AppConfigServiceTests.cs
+++ Tests/Services/AppConfigServiceTests.cs
@@ -1,122 +1,267 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Moq;
 using NUnit.Framework;
 using TN_DocGeneral.Services;
+using TN.DocData;
+using Newtonsoft.Json;
 
 namespace Tests.Services
 {
     [TestFixture]
     public class AppConfigServiceTests
     {
-        private Mock<IConfiguration> _configurationMock;
+        private IConfiguration _configuration;
         private AppConfigService _appConfigService;
+        private string _testBasePath;
+        private string _testCfgPath;
+        private string _testCfgAppPath;
+        private string _testLastUsedTemplateListPath;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _testBasePath = Path.Combine(Path.GetTempPath(), "AppConfigServiceTests");
+            Directory.CreateDirectory(_testBasePath);
+            Directory.CreateDirectory(Path.Combine(_testBasePath, "Cfg"));
+            Directory.CreateDirectory(Path.Combine(_testBasePath, "UserPreference"));
+            
+            // Создаем пути к тестовым файлам конфигурации
+            _testCfgPath = Path.Combine(_testBasePath, "Cfg", "Cfg.json");
+            _testCfgAppPath = Path.Combine(_testBasePath, "Cfg", "CfgApp.json");
+            _testLastUsedTemplateListPath = Path.Combine(_testBasePath, "UserPreference", "LastUsedTemplateList.json");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            if (Directory.Exists(_testBasePath))
+            {
+                Directory.Delete(_testBasePath, true);
+            }
+        }
 
         [SetUp]
         public void Setup()
         {
-            _configurationMock = new Mock<IConfiguration>();
-            _appConfigService = new AppConfigService(_configurationMock.Object);
-        }
-
-        [Test]
-        public async Task GetConfig_ShouldReturnConfig()
-        {
-            // Arrange
-            var expectedConfig = new Dictionary<string, string>
+            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            
+            var configurationBuilder = new ConfigurationBuilder();
+            var configDictionary = new Dictionary<string, string>
             {
-                { "Key1", "Value1" },
-                { "Key2", "Value2" }
+                {"CfgDirPath", Path.Combine(_testBasePath, "Cfg")},
+                {"RelCfgName", "Cfg.json"},
+                {"RelCfgAppName", "CfgApp.json"},
+                {"UserPreferenceDirPath", Path.Combine(_testBasePath, "UserPreference")},
+                {"LastUsedTemplateListFileName", "LastUsedTemplateList.json"}
+            };
+            
+            configurationBuilder.AddInMemoryCollection(configDictionary);
+            _configuration = configurationBuilder.Build();
+
+            // Создаем тестовые данные
+            var root = new Root();
+            var cfgApp = new CfgApp 
+            { 
+                Devices = new List<Device> 
+                { 
+                    new Device 
+                    { 
+                        IdDevice = 1, 
+                        Name = "Test Device",
+                        Docs = new List<Document>
+                        {
+                            new Document
+                            {
+                                IdDoc = IdDoc.Passport,
+                                LastUsedTemplateId = 1,
+                                TemplateDocs = new List<TemplateDoc>
+                                {
+                                    new TemplateDoc { Id = 1, Name = "Test Template" }
+                                }
+                            }
+                        },
+                        Elis = new Elis { Use = true }
+                    } 
+                },
+                Elis = new Elis { Use = true }
+            };
+            var lastUsedTemplateList = new LastUsedTemplateListCfg
+            {
+                Devices = new List<LastUsedTemplateList>
+                {
+                    new LastUsedTemplateList
+                    {
+                        IdDevice = 1,
+                        LastTemplateList = new List<LastUsedTemplate>
+                        {
+                            new LastUsedTemplate
+                            {
+                                IdDoc = IdDoc.Passport,
+                                LastTemplateId = 1
+                            }
+                        }
+                    }
+                }
             };
 
-            var configSectionMock = new Mock<IConfigurationSection>();
-            configSectionMock.Setup(x => x.Value).Returns("Value1");
-            _configurationMock.Setup(x => x.GetSection("Key1")).Returns(configSectionMock.Object);
+            // Записываем тестовые данные в файлы
+            File.WriteAllText(_testCfgPath, JsonConvert.SerializeObject(root));
+            File.WriteAllText(_testCfgAppPath, JsonConvert.SerializeObject(cfgApp));
+            File.WriteAllText(_testLastUsedTemplateListPath, JsonConvert.SerializeObject(lastUsedTemplateList));
 
-            // Act
-            var result = await _appConfigService.GetConfig();
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
 
-            // Assert
-            Assert.That(result, Is.Not.Null);
-            Assert.That(result, Is.InstanceOf<Dictionary<string, string>>());
-            _configurationMock.Verify(x => x.GetSection(It.IsAny<string>()), Times.AtLeastOnce);
+            _appConfigService = AppConfigService.GetInstance(_configuration);
         }
 
         [Test]
-        public async Task GetConfig_WithEmptyConfiguration_ShouldReturnEmptyDictionary()
+        public void GetInstance_WithNullConfiguration_ShouldThrowArgumentNullException()
         {
-            // Arrange
-            _configurationMock.Setup(x => x.GetSection(It.IsAny<string>())).Returns((IConfigurationSection)null);
-
-            // Act
-            var result = await _appConfigService.GetConfig();
-
-            // Assert
-            Assert.That(result, Is.Not.Null);
-            Assert.That(result.Count, Is.EqualTo(0));
-        }
-
-        [Test]
-        public void GetConfig_WithNullConfiguration_ShouldThrowArgumentNullException()
-        {
-            // Arrange
-            var nullConfigService = new AppConfigService(null);
-
             // Act & Assert
-            Assert.ThrowsAsync<ArgumentNullException>(async () => await nullConfigService.GetConfig());
+            Assert.Throws<ArgumentNullException>(() => AppConfigService.GetInstance(null));
         }
 
         [Test]
-        public async Task GetConfig_WithInvalidConfiguration_ShouldHandleException()
+        public void GetInstance_WithValidConfiguration_ShouldReturnSameInstance()
         {
-            // Arrange
-            _configurationMock.Setup(x => x.GetSection(It.IsAny<string>()))
-                .Throws(new Exception("Configuration error"));
-
             // Act
-            var result = await _appConfigService.GetConfig();
+            var instance1 = AppConfigService.GetInstance(_configuration);
+            var instance2 = AppConfigService.GetInstance(_configuration);
+
+            // Assert
+            Assert.That(instance1, Is.SameAs(instance2));
+        }
+
+        [Test]
+        public void GetCfg_ShouldReturnRoot()
+        {
+            // Act
+            var result = _appConfigService.GetCfg();
 
             // Assert
             Assert.That(result, Is.Not.Null);
-            Assert.That(result.Count, Is.EqualTo(0));
+            Assert.That(result, Is.InstanceOf<Root>());
         }
 
         [Test]
-        public async Task GetConfig_WithMultipleSections_ShouldReturnAllValues()
+        public void GetAppCfg_ShouldReturnCfgApp()
         {
-            // Arrange
-            var configSection1Mock = new Mock<IConfigurationSection>();
-            configSection1Mock.Setup(x => x.Value).Returns("Value1");
-            var configSection2Mock = new Mock<IConfigurationSection>();
-            configSection2Mock.Setup(x => x.Value).Returns("Value2");
-
-            _configurationMock.Setup(x => x.GetSection("Key1")).Returns(configSection1Mock.Object);
-            _configurationMock.Setup(x => x.GetSection("Key2")).Returns(configSection2Mock.Object);
-
             // Act
-            var result = await _appConfigService.GetConfig();
+            var result = _appConfigService.GetAppCfg();
 
             // Assert
-            Assert.That(result.Count, Is.EqualTo(2));
-            Assert.That(result["Key1"], Is.EqualTo("Value1"));
-            Assert.That(result["Key2"], Is.EqualTo("Value2"));
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<CfgApp>());
+            Assert.That(result.Devices, Has.Count.EqualTo(1));
+            Assert.That(result.Devices[0].IdDevice, Is.EqualTo(1));
         }
 
         [Test]
-        public async Task GetConfig_WithNullSectionValue_ShouldHandleNullValue()
+        public void GetDeviceCfg_WithInvalidId_ShouldReturnNull()
         {
-            // Arrange
-            var configSectionMock = new Mock<IConfigurationSection>();
-            configSectionMock.Setup(x => x.Value).Returns((string)null);
-            _configurationMock.Setup(x => x.GetSection("Key1")).Returns(configSectionMock.Object);
-
             // Act
-            var result = await _appConfigService.GetConfig();
+            var result = _appConfigService.GetDeviceCfg(-1);
 
             // Assert
-            Assert.That(result["Key1"], Is.Null);
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void GetDocCfg_WithInvalidIds_ShouldReturnNull()
+        {
+            // Act
+            var result = _appConfigService.GetDocCfg(-1, IdDoc.Passport);
+
+            // Assert
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void GetLastUsedTemplateList_ShouldReturnLastUsedTemplateListCfg()
+        {
+            // Act
+            var result = _appConfigService.GetLastUsedTemplateList();
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<LastUsedTemplateListCfg>());
+            Assert.That(result.Devices, Has.Count.EqualTo(1));
+            Assert.That(result.Devices[0].LastTemplateList, Has.Count.EqualTo(1));
+        }
+
+        [Test]
+        public void GetLastUsedTemplateId_WithInvalidIds_ShouldReturnNull()
+        {
+            // Act
+            var result = _appConfigService.GetLastUsedTemplateId(-1, IdDoc.Passport);
+
+            // Assert
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void SetLastUsedTemplateId_WithInvalidIds_ShouldReturnFalse()
+        {
+            // Act
+            var result = _appConfigService.SetLastUsedTemplateId(-1, IdDoc.Passport, 1);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void GetPathToDocDll_WithInvalidIds_ShouldReturnNull()
+        {
+            // Act
+            var result = _appConfigService.GetPathToDocDll(-1, IdDoc.Passport);
+
+            // Assert
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void GetPathConfigFile_WithInvalidIds_ShouldReturnEmptyString()
+        {
+            // Act
+            var result = _appConfigService.GetPathConfigFile(-1, IdDoc.Passport);
+
+            // Assert
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void GetPathEditConfigFile_WithInvalidIds_ShouldReturnEmptyString()
+        {
+            // Act
+            var result = _appConfigService.GetPathEditConfigFile(-1, IdDoc.Passport);
+
+            // Assert
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void GetPathTemplateFile_WithInvalidIds_ShouldReturnEmptyString()
+        {
+            // Act
+            var result = _appConfigService.GetPathTemplateFile(-1, IdDoc.Passport);
+
+            // Assert
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void SetElisClientToken_WithInvalidDeviceId_ShouldReturnFalse()
+        {
+            // Act
+            var result = _appConfigService.SetElisClientToken(-1, "test-token");
+
+            // Assert
+            Assert.That(result, Is.False);
         }
     }
 } 
