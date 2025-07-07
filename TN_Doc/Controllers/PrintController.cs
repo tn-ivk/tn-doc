977d06fb4f9cb643a31c4060ae6523b9ad179462
DadonovAleksandr <a.snafu@ya.ru>
2025-07-07 20:52:25 +0300

    удалил хлам

--- TN_Doc/Controllers/PrintController.cs
+++ TN_Doc/Controllers/PrintController.cs
@@ -68,59 +68,5 @@ namespace TN_Doc.Controllers
             await _service.PrintDocAsync(printerName);
             return Ok();
         }
-
-        #region old_code
-
-                // public List<string> GetListPrinters()
-        // {
-        //     List<string> printers = new List<string>();
-        //     var process = new Process()
-        //     {
-        //         StartInfo =
-        //         {
-        //             RedirectStandardOutput = true,
-        //             RedirectStandardError = true,
-        //             UseShellExecute = false,
-        //             WindowStyle = ProcessWindowStyle.Hidden,
-        //             Arguments = "-e",
-        //             FileName = "lpstat",
-        //             CreateNoWindow = true,
-        //             
-        //         }
-        //     };
-        //     process.Start();
-        //     process.WaitForExit();
-        //     var resultOutput = process.StandardOutput.ReadToEnd();
-        //     foreach (var item in resultOutput.Split('\n'))
-        //         if (!string.IsNullOrEmpty(item))
-        //             printers.Add(item);
-        //     
-        //     return printers;
-        // }
-        //
-        // public void PrintDoc(string printerName)
-        // {
-        //     var printersName = GetListPrinters();
-        //     Console.WriteLine($"-H localhost -P  {printerName} -ol {Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot", "PDF", $"PDF.pdf")}");
-        //     if (printersName.Contains(printerName))
-        //     {
-        //         var process = new Process()
-        //         {
-        //             StartInfo =
-        //             {
-        //                 UseShellExecute = false,
-        //                 WindowStyle = ProcessWindowStyle.Hidden,
-        //                 Arguments
-        //                     = $"-H localhost -P  {printerName} -ol {Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot", "PDF", $"PDF.pdf")}",
-        //                 FileName = "lpr",
-        //                 CreateNoWindow = true
-        //             }
-        //         };
-        //         process.Start();
-        //         process.WaitForExit();
-        //     }
-        //     
-
-        #endregion
     }
 }
