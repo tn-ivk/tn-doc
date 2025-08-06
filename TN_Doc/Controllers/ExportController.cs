84c8a717beabc4ab453aa8362816049691a2c9da
DadonovAleksandr <a.snafu@ya.ru>
2025-08-06 09:14:50 +0300

    Рефакторинг ExportController

--- TN_Doc/Controllers/ExportController.cs
+++ TN_Doc/Controllers/ExportController.cs
@@ -4,22 +4,19 @@ using System.Collections.Generic;
 using System.IO;
 using TN.DocData;
 
-namespace TN_Doc.Controllers
+namespace TN_Doc.Controllers;
+
+public class ExportController : Controller
 {
-    public class ExportController : Controller
+    private readonly ILogger<PrintController> _logger;
+
+    public List<string> GetListFormats()
     {
-        private readonly ILogger<PrintController> _logger;
+        return new List<string>() { "pdf", "excel", "ods", "xml" };
+    }
 
-        public List<string> GetListFormats()
-        {
-            return new List<string>() { "pdf", "excel", "ods", "xml" };
-        }
-
-        public void ExportDoc(int IdDevice, IdDoc IdDoc, int id, string format)
-        {
-            //FileInfo fileInfo = new FileInfo(format);
-            //File.Copy("", "", true);
-            System.IO.File.Copy(Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot", "PDF", $"PDF.pdf"), Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot", "PDF", $"PDF2.pdf"), false);
-        }
+    public void ExportDoc(int IdDevice, IdDoc IdDoc, int id, string format)
+    {
+        System.IO.File.Copy(Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot", "PDF", $"PDF.pdf"), Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot", "PDF", $"PDF2.pdf"), false);
     }
 }
