712f02793eecbcec499867e4702b98dc71d7aa0b
DadonovAleksandr <a.snafu@ya.ru>
2025-08-24 00:37:13 +0300

    Мелкие правки

--- TN_Doc/Models/Printer/LinuxPrinter.cs
+++ TN_Doc/Models/Printer/LinuxPrinter.cs
@@ -1,73 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using TN_Doc.Models.Services;
 using System.Threading.Tasks;
 
-namespace TN_Doc.Models.Printer
-{
-    /// <summary>
-    /// Класс принтера для взаимодействия с печатью под Linux
-    /// </summary>
-    public sealed class LinuxPrinter : AbsPrinter
-    {
-        private readonly IReportBuffer _buffer;
+namespace TN_Doc.Models.Printer;
 
-        public LinuxPrinter(IReportBuffer buffer)
+/// <summary>
+/// Класс принтера для взаимодействия с печатью под Linux
+/// </summary>
+public sealed class LinuxPrinter : AbsPrinter
+{
+    private readonly IReportBuffer _buffer;
+
+    public LinuxPrinter(IReportBuffer buffer)
+    {
+        _buffer = buffer;
+    }
+    /// <summary>
+    /// Получение списка доступных принтеров в системе
+    /// </summary>
+    /// <returns>Список доступных принтеров</returns>
+    public override IEnumerable<string> GetAvailablePrinters()
+    {
+        using var process = new Process();
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        process.StartInfo.Arguments = "-e";
+        process.StartInfo.FileName = "lpstat";
+        process.StartInfo.CreateNoWindow = true;
+        process.Start();
+        process.WaitForExit();
+        foreach (var item in process.StandardOutput.ReadToEnd().Split('\n', StringSplitOptions.RemoveEmptyEntries))
+            yield return item;
+    }
+
+    /// <summary>
+    /// Печать сформированного отчёта на заданном принтере
+    /// </summary>
+    /// <param name="printerName">Название принтера</param>
+    public override Task PrintDocAsync(string printerName)
+    {
+        return Task.Run(() =>
         {
-            _buffer = buffer;
-        }
-        /// <summary>
-        /// Получение списка доступных принтеров в системе
-        /// </summary>
-        /// <returns>Список доступных принтеров</returns>
-        public override IEnumerable<string> GetAvailablePrinters()
-        {
+            var printersName = GetAvailablePrinters();
+            if (!printersName.Contains(printerName))
+                return;
+            var pdfBytes = _buffer.GetPdfBytes();
+            if (pdfBytes is null || pdfBytes.Length == 0)
+                return;
             using var process = new Process();
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.RedirectStandardError = true;
             process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardInput = true;
             process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            process.StartInfo.Arguments = "-e";
-            process.StartInfo.FileName = "lpstat";
+            process.StartInfo.Arguments = $"-d {printerName} -t \"TN_Doc\"";
+            process.StartInfo.FileName = "lp";
             process.StartInfo.CreateNoWindow = true;
             process.Start();
-            process.WaitForExit();
-            foreach (var item in process.StandardOutput.ReadToEnd().Split('\n', StringSplitOptions.RemoveEmptyEntries))
-                yield return item;
-        }
-
-        /// <summary>
-        /// Печать сформированного отчёта на заданном принтере
-        /// </summary>
-        /// <param name="printerName">Название принтера</param>
-        public override Task PrintDocAsync(string printerName)
-        {
-            return Task.Run(() =>
+            using (var stdin = process.StandardInput.BaseStream)
             {
-                var printersName = GetAvailablePrinters();
-                if (!printersName.Contains(printerName))
-                    return;
-                var pdfBytes = _buffer.GetPdfBytes();
-                if (pdfBytes is null || pdfBytes.Length == 0)
-                    return;
-                using var process = new Process();
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.RedirectStandardInput = true;
-                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                process.StartInfo.Arguments = $"-d {printerName} -t \"TN_Doc\"";
-                process.StartInfo.FileName = "lp";
-                process.StartInfo.CreateNoWindow = true;
-                process.Start();
-                using (var stdin = process.StandardInput.BaseStream)
-                {
-                    stdin.Write(pdfBytes, 0, pdfBytes.Length);
-                    stdin.Flush();
-                }
-                process.WaitForExit();
-            });
-        }
+                stdin.Write(pdfBytes, 0, pdfBytes.Length);
+                stdin.Flush();
+            }
+            process.WaitForExit();
+        });
     }
 }
--- TN_Doc/Models/Services/PrinterService.cs
+++ TN_Doc/Models/Services/PrinterService.cs
@@ -2,33 +2,32 @@
 using System.Threading.Tasks;
 using TN_Doc.Models.Printer;
 
-namespace TN_Doc.Models.Services
+namespace TN_Doc.Models.Services;
+
+/// <summary>
+/// Сервис для взаимодействия с принтерами
+/// </summary>
+public sealed class PrinterService
 {
+    private readonly AbsPrinter _printer;
+
     /// <summary>
-    /// Сервис для взаимодействия с принтерами
+    /// Инициализация сервиса
     /// </summary>
-    public sealed class PrinterService
-    {
-        private readonly AbsPrinter _printer;
+    /// <param name="printer">Конкретная реализация агента работы с принтером</param>
+    public PrinterService(AbsPrinter printer) => _printer = printer;
+    
+    /// <summary>
+    /// Получение списка принтеров для печати
+    /// </summary>
+    /// <returns>
+    /// Список принтеров
+    /// </returns>
+    public IEnumerable<string> GetPrinters() => _printer.GetAvailablePrinters();
 
-        /// <summary>
-        /// Инициализация сервиса
-        /// </summary>
-        /// <param name="printer">Конкретная реализация агента работы с принтером</param>
-        public PrinterService(AbsPrinter printer) => _printer = printer;
-        
-        /// <summary>
-        /// Получение списка принтеров для печати
-        /// </summary>
-        /// <returns>
-        /// Список принтеров
-        /// </returns>
-        public IEnumerable<string> GetPrinters() => _printer.GetAvailablePrinters();
-
-        /// <summary>
-        /// Печать документа на конкретном принтере
-        /// </summary>
-        /// <param name="printerName">Список доступных принтеров</param>
-        public async Task PrintDocAsync(string printerName) => await _printer.PrintDocAsync(printerName);
-    }
+    /// <summary>
+    /// Печать документа на конкретном принтере
+    /// </summary>
+    /// <param name="printerName">Список доступных принтеров</param>
+    public async Task PrintDocAsync(string printerName) => await _printer.PrintDocAsync(printerName);
 }
