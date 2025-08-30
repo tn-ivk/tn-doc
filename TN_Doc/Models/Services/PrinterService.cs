13de7d0357759f77401860631a3f97ecc4d75f5e
DadonovAleksandr <a.snafu@ya.ru>
2025-08-30 21:12:11 +0300

    Рефакторинг PrinterService

--- TN_Doc/Models/Services/PrinterService.cs
+++ TN_Doc/Models/Services/PrinterService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using TN_Doc.Models.Printer;
 
 namespace TN_Doc.Models.Services;
@@ -12,17 +11,14 @@ namespace TN_Doc.Models.Services;
 public sealed class PrinterService : IPrinterService
 {
     private readonly AbsPrinter _printer;
-    private readonly ILogger<PrinterService> _logger;
 
     /// <summary>
     /// Инициализация сервиса
     /// </summary>
     /// <param name="printer">Конкретная реализация агента работы с принтером</param>
-    /// <param name="logger">Логгер для записи ошибок</param>
-    public PrinterService(AbsPrinter printer, ILogger<PrinterService> logger)
+    public PrinterService(AbsPrinter printer)
     {
         _printer = printer ?? throw new ArgumentNullException(nameof(printer));
-        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
     
     /// <summary>
@@ -31,33 +27,11 @@ public sealed class PrinterService : IPrinterService
     /// <returns>
     /// Список принтеров
     /// </returns>
-    public IEnumerable<string> GetPrinters()
-    {
-        try
-        {
-            return _printer.GetAvailablePrinters();
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "Ошибка при получении списка принтеров");
-            throw;
-        }
-    }
+    public IEnumerable<string> GetPrinters() => _printer.GetAvailablePrinters();
 
     /// <summary>
     /// Печать документа на конкретном принтере
     /// </summary>
     /// <param name="printerName">Список доступных принтеров</param>
-    public async Task PrintDocAsync(string printerName)
-    {
-        try
-        {
-            await _printer.PrintDocAsync(printerName);
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "Ошибка при печати документа на принтере {PrinterName}", printerName);
-            throw;
-        }
-    }
+    public async Task PrintDocAsync(string printerName) => await _printer.PrintDocAsync(printerName);
 }
