353444ddbe3b97afbe4d66c2db97b91d83ad44c4
DadonovAleksandr <a.snafu@ya.ru>
2025-09-30 22:44:45 +0300

    cor

--- TN_Doc/Services/StatusMonitoringService.cs
+++ TN_Doc/Services/StatusMonitoringService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using TN_Doc.Hubs;
