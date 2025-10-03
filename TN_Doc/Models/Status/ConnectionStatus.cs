3e72182191305e679b3a81226abf27af1d3fc4bc
DadonovAleksandr <a.snafu@ya.ru>
2025-10-03 10:43:26 +0300

    refactoring

--- TN_Doc/Models/Status/ConnectionStatus.cs
+++ TN_Doc/Models/Status/ConnectionStatus.cs
@@ -1,15 +1,14 @@
 using System;
 
-namespace TN_Doc.Models.Status
+namespace TN_Doc.Models.Status;
+
+/// <summary>
+/// Статус подключения к внешнему сервису
+/// </summary>
+public class ConnectionStatus
 {
-    /// <summary>
-    /// Статус подключения к внешнему сервису
-    /// </summary>
-    public class ConnectionStatus
-    {
-        public bool IsConnected { get; set; }
-        public int? LatencyMs { get; set; }
-        public DateTime? LastChecked { get; set; }
-        public string? Error { get; set; }
-    }
+    public bool IsConnected { get; set; }
+    public int? LatencyMs { get; set; }
+    public DateTime? LastChecked { get; set; }
+    public string Error { get; set; }
 }
--- TN_Doc/Models/Status/StatusResponse.cs
+++ TN_Doc/Models/Status/StatusResponse.cs
@@ -1,15 +1,14 @@
 using System;
 using System.Collections.Generic;
 
-namespace TN_Doc.Models.Status
+namespace TN_Doc.Models.Status;
+
+/// <summary>
+/// Ответ со статусом всех систем
+/// </summary>
+public class StatusResponse
 {
-    /// <summary>
-    /// Ответ со статусом всех систем
-    /// </summary>
-    public class StatusResponse
-    {
-        public List<DeviceStatus> Devices { get; set; } = new();
-        public ServiceStatus Services { get; set; } = new();
-        public string Timestamp { get; set; } = DateTime.UtcNow.ToString("o");
-    }
+    public List<DeviceStatus> Devices { get; set; } = new();
+    public ServiceStatus Services { get; set; } = new();
+    public string Timestamp { get; set; } = DateTime.UtcNow.ToString("o");
 }
