f4e4034cb45cf52c85a8c6a7c99551798bfbb512
DadonovAleksandr <a.snafu@ya.ru>
2025-12-07 22:49:21 +0300

    phase 1

---
+++ TN_Doc/Client/configurator/src/components/visual-editors/PassportConfigEditor.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { PassportEditConfig } from '@/types/passport-config.types';
+
+interface Props {
+  config: PassportEditConfig;
+  configPath: string;
+}
+
+interface Emits {
+  (e: 'update:config', config: PassportEditConfig): void;
+}
+
+defineProps<Props>();
+defineEmits<Emits>();
+
+// TODO: Реализация на этапе 3
+</script>
+
+<template>
+  <div class="passport-config-editor">
+    <p>Визуальный редактор паспорта (в разработке)</p>
+  </div>
+</template>
+
+<style scoped>
+.passport-config-editor {
+  padding: 1rem;
+}
+</style>
